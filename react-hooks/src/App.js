db899b790d342e54290cc6d92a1338fd65cce4fb
Jakkani <13.naveen@gmail.com>
2020-09-07 15:59:18 +0530

    react reducers examples are added.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
+import React, { useReducer, useContext } from "react";
+import "./App.css";
+import ComponentC from "./components/hook-context/ComponentC";
+import ReducerCounter from "./components/ReducerCounter";
 
+import CompA from "./components/reducer-context/CompA";
+import CompB from "./components/reducer-context/CompB";
+import CompC from "./components/reducer-context/CompC";
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
+export const CounterContext = React.createContext();
+
+const initialState = { count : 0, name:'Jakkani'};
+
+export const counterReducer = (state, action) => {
+  // console.log('Reducer--- ', action)
+  switch (action) {
+    case "inc":
+      console.log(state.count)
+      return {...state,  count : state.count + 1};
+    case "dec":
+      return {...state,  count : state.count - 1};
+    case "reset":
+      return initialState;
+    default:
+      break;
+  }
+};
+
+const name = { fname: "Naveen Kumar", lname: "Jakkani" };
 function App() {
+  const [countObject, dispatch] = useReducer(counterReducer, initialState);
   return (
     <div className="App">
-     {/* <ClassCounter/>
-     <HookCounter /> */}
-     <HookCounter3 />
+      {/*  <UserContext.Provider value={name} >
+        <ChannelContext.Provider value={'React Learning Channel'}>
+        <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+
+      {/* <ReducerCounter />
+       */}
+      <CounterContext.Provider
+        value={{ countDispatch: dispatch, reducerCounter: countObject}}>
+        <CompA />
+        <CompB />
+        <CompC />
+      </CounterContext.Provider>
     </div>
   );
 }
